b395fff4989f2b9c7c3b09a77f6dc7fed902bad9
Stephen Ross <ross8328@gwmail.gwu.edu>
2017-01-07 16:18:04 -0600

    Update

--- NumberGuessingGame.js
+++ NumberGuessingGame.js
@@ -1,7 +1,25 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var guess = prompt("I'm thinking of a number between 1 and 6. What could it be?");
-if (parseInt(guess) === randomNumber) {
-	document.write('<p>You guessed the number!</p>');
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+function getRandomNumber(upper){
+	var num = Math.floor(Math.random() + upper) + 1;
+	return num;
+}
+
+while (guessCount < 10){
+	guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+	guessCount += 1;
+	if (parseInt(guess) === randomNumber) {
+		correctGuess = true;
+		break;
+	}
+}
+
+if (correctGuess) {
+	document.write('<h1> Your guessed the number!</h1>');
+	document.write('It tood you' + guessCount + ' tries to guess the number ' + randomNumber);
 } else {
-	document.write('<p>Sorry the number was ' + randomNumber + ' . Try again! </p>');
+	document.write('<h1> Sorry, It took you too many tries to guess the number.</h1>');
 }
